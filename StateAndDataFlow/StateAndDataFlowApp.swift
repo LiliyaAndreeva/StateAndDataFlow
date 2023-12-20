793f4abde0a45d15a033ebc69f4554d139a3e8ce
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-12-20 19:35:40 +0500

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Лилия Андреева on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
